b64ff395b8393d09d724e7cc4eeda1c2b4391d13
dbc2201 <divyanshb22@gmail.com>
2020-01-17 12:02:52 +0530

    set the rate of interest of IOB is 3%

---
+++ src/iobBank/IOBBank.java
@@ -0,0 +1,24 @@
+/*
+ *   Created by IntelliJ IDEA Ultimate, 2020
+ *   User: dbc2201
+ *   Date: 17/01/20
+ *   Time: 12:02 PM
+ */
+
+package iobBank;
+
+import rbi.Bank;
+
+public class IOBBank implements Bank {
+
+    @Override
+    public int getRateOfInterest() {
+        // the rate of interest of IOB Bank is 3%
+        return 3;
+    }
+
+    @Override
+    public int getFixedDepositInterest() {
+        return 0;
+    }
+}
