7d36f12392c19ed210863f18f195389ad0b4864a
dbc2201 <divyanshb22@gmail.com>
2020-01-17 12:08:07 +0530

    add JavaDoc comment for getRateOfInterest

--- src/rbi/Bank.java
+++ src/rbi/Bank.java
@@ -5,6 +5,12 @@ package rbi;
  * In Java, we declare the ADT with the help of an Interface.
  */
 public interface Bank {
+    /**
+     * This method returns the rate of interest in a savings account for a bank.
+     * The value can be different for different banks.
+     *
+     * @return The rate of interest of savings account in the bank
+     */
     int getRateOfInterest();
 
     int getFixedDepositInterest();
