db655e37786d34c4d6120356837ef2e1046b6a63
dbc2201 <divyanshb22@gmail.com>
2020-01-17 12:09:11 +0530

    add JavaDoc comment for getFixedDepositInterest()

--- src/rbi/Bank.java
+++ src/rbi/Bank.java
@@ -9,9 +9,15 @@ public interface Bank {
      * This method returns the rate of interest in a savings account for a bank.
      * The value can be different for different banks.
      *
-     * @return The rate of interest of savings account in the bank
+     * @return The rate of interest of savings account in the bank.
      */
     int getRateOfInterest();
 
+    /**
+     * This method returns the rate of interest in a fixed deposit account for a bank.
+     * The value can be different for different banks.
+     *
+     * @return The rate of interest of a fixed deposit account in the bank.
+     */
     int getFixedDepositInterest();
 }
