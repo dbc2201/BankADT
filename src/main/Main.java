1492a06ef6662b12707c338bae75130810e05b92
dbc2201 <divyanshb22@gmail.com>
2020-01-17 12:15:38 +0530

    add documentation comments

--- src/main/Main.java
+++ src/main/Main.java
@@ -14,8 +14,11 @@ public class Main {
     public static void main(String[] args) {
         // create an object of the ICICI Bank class
         ICICIBank iciciBankMathuraBranch = new ICICIBank();
+        // print the rate of interest of the ICICI Bank Savings Account
         System.out.println("Rate of interest of ICICI Bank is: " + iciciBankMathuraBranch.getRateOfInterest());
+        // create an object of the IOB Bank class
         IOBBank iobBankGLABranch = new IOBBank();
+        // print the rate of interest of the IOB Bank Savings Account
         System.out.println("Rate of interest of IOB Bank is: " + iobBankGLABranch.getRateOfInterest());
     }
 }
