aa7171bbf69c2c5b500d11175d39acbbdd0ad2e3
dbc2201 <divyanshb22@gmail.com>
2020-01-17 12:00:17 +0530

    make the class ICICIBank implement the interface Bank and override the methods

--- src/iciciBank/ICICIBank.java
+++ src/iciciBank/ICICIBank.java
@@ -7,5 +7,16 @@
 
 package iciciBank;
 
-public class ICICIBank {
+import rbi.Bank;
+
+public class ICICIBank implements Bank {
+    @Override
+    public int getRateOfInterest() {
+        return 0;
+    }
+
+    @Override
+    public int getFixedDepositInterest() {
+        return 0;
+    }
 }
