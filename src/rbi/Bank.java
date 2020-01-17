16c2b9c55e7f0847b09a6148c4b717d88b27857f
dbc2201 <divyanshb22@gmail.com>
2020-01-17 12:07:10 +0530

    add JavaDoc comment for Bank ADT

--- src/rbi/Bank.java
+++ src/rbi/Bank.java
@@ -1,5 +1,9 @@
 package rbi;
 
+/**
+ * This is the ADT (Abstract Data Type) of a Bank.
+ * In Java, we declare the ADT with the help of an Interface.
+ */
 public interface Bank {
     int getRateOfInterest();
 
